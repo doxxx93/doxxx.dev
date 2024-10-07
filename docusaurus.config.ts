2f38267d021ec668f15b38826cbabcdcc5f36ae5
doxxx93 <doxxx93@gmail.com>
2024-10-07 18:38:00 +0900

    first commit

---
+++ docusaurus.config.ts
@@ -0,0 +1,126 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
+const config: Config = {
+  title: 'Doxxx Dev',
+  tagline: 'Take no prisoner',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://doxxx.dev',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'doxxx93', // Usually your GitHub org/user name.
+  projectName: 'doxxx.dev', // Usually your repo name.
+  trailingSlash: false,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'kr',
+    locales: ['kr'],
+  },
+
+  // Plugins
+  plugins: ['docusaurus-plugin-sass'],
+
+  // mermaid configuration
+  markdown: {
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
+
+  // katex stylesheets
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/doxxx93.doxxx.dev/blob/master/',
+        },
+        blog: {
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/doxxx93.doxxx.dev/blob/master/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: ('./src/css/custom.scss'),
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Doxxx Dev',
+      logo: {
+        alt: 'Doxxx profile logo',
+        src: 'img/logo.PNG',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()}  Doyul Kim, All Rights Reserved.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+      additionalLanguages: ['java', 'bash', 'json', 'sql', 'toml', 'docker', 'yaml'],
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
